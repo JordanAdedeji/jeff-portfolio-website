88c0facb586cccd4a63b0ee75d05b59f40f2b284
Jordan Adedeji <jdon4692@gmail.com>
2023-07-19 22:05:00 +0100

    add videos

--- src/components/productList/ProductList.jsx
+++ src/components/productList/ProductList.jsx
@@ -7,6 +7,12 @@ import trim from "../../img/trim.mp4"
 import bet365 from "../../img/bet365.mp4"
 import chai from "../../img/chai.mp4"
 
+import converse from "../../img/converse.mp4"
+import final from "../../img/final.mp4"
+import gamesVideo from "../../img/games-video.mp4"
+import sixScores from "../../img/sixScores.mp4"
+import winnings from "../../img/winnings.mp4"
+
 
 const ProductList = () => {
   return (
@@ -23,6 +29,17 @@ const ProductList = () => {
       <div className="pl-list2">
         <Product videoImport={guard} />
         <Product videoImport={bet365} />
+      </div>
+      <div className="pl-list11">
+        <Product videoImport={sixScores} />
+        <Product videoImport={winnings} />
+      </div>
+      <div className="pl-list22">
+        <Product videoImport={gamesVideo} />
+        <Product videoImport={converse} />
+      </div>
+      <div className="pl-list4">
+        <Product videoImport={final} />
       </div>
     </div>
   );
